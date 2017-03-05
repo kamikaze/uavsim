96560e2c306436e757e4b3ce357944476a5b5398
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2017-03-05 07:13:30 +0200

    serial: sending groundspeed

--- tools/serial_transceiver.py
+++ tools/serial_transceiver.py
@@ -68,8 +68,10 @@ def generate_nmea_sentences(telemetry):
     # yaw-deg is '' for some reason
     yaw_z = heading
 
+    speed_over_ground = telemetry['groundspeed-kt']
+
     gpgga = '$GPGGA,{}.000,{:09.4f},{},{:010.4f},{},1,7,1.15,{},M,23.7,M,,*6F'.format(t, lat, lat_half, lon, lon_half, alt)
-    gprmc = '$GPRMC,{}.000,A,{:09.4f},{},{:010.4f},{},0.03,267.70,{},,,A*6D'.format(t, lat, lat_half, lon, lon_half, d)
+    gprmc = '$GPRMC,{}.000,A,{:09.4f},{},{:010.4f},{},{:.2f},267.70,{},,,A*6D'.format(t, lat, lat_half, lon, lon_half, speed_over_ground, d)
     exinj = '$EXINJ,{},{},{},{},NA'.format(heading, roll_x, pitch_y, yaw_z)
 
     return [gpgga, gprmc, exinj]
@@ -104,6 +106,7 @@ def read_fg_data(telnet_client, path):
 def read_fg_telemetry(telnet_client):
     telemetry = read_fg_data(telnet_client, 'position')
     telemetry.update(read_fg_data(telnet_client, 'orientation/model'))
+    telemetry.update(read_fg_data(telnet_client, 'velocities'))
 
     return telemetry
 
